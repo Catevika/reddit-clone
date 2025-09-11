8bade54024ae73e5d7a384f12fdded9ffee3fd45
Catevika <dominique.bello@outlook.fr>
2025-09-11 16:59:33 +0200

    Add PostListItem + HomeScreen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
-import {Text, View} from 'react-native';
+import posts from '@/assets/data/posts.json';
+import PostListItem from '@/src/components/PostListItem';
+import {FlatList} from 'react-native';
 
 export default function HomeScreen() {
 	return (
-		<View>
-			<Text>HomeScreen</Text>
-		</View>
+		<FlatList
+			data={posts}
+			renderItem={({item}) => <PostListItem post={item} />}
+			keyExtractor={(item) => item.id}
+			contentContainerStyle={{padding: 16}}
+		/>
 	);
 }
